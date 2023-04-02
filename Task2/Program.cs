14635a7679f2d7f03581be3c23d72f3e64e8a981
Sergei821 <ps16111982@gmail.com>
2023-04-02 10:51:05 +0300

    задача 2 решена

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+int ReadInt(string message)
+{
+    System.Console.Write($"{message} > ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int fuctDigits(int number)
+{
+    int fuct = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        fuct = fuct * i;
+    }
+    return fuct;
+}
+
+int value;
+value = ReadInt("Введите число");
+int fuct = fuctDigits(value);
+System.Console.WriteLine($"Произведение чисел от 1 до N равно {fuct}");
+
